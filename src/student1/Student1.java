97bc358953ff6e458255f18dc8ccc3766de022b9
Bhavya <shahbha@shernet.sheridancollege.ca>
2023-01-24 10:19:58 -0500

    Added class variable address

--- src/student1/Student1.java
+++ src/student1/Student1.java
@@ -15,7 +15,7 @@ public class Student1 {
      */
   private String studentId;
   private String studentName;
-
+  private String address;
     public Student1(String studentId, String studentName) {
         this.studentId = studentId;
         this.studentName = studentName;
@@ -36,6 +36,15 @@ public class Student1 {
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
 }
     
 
